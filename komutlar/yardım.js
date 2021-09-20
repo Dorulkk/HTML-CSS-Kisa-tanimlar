2442fce518b74cfea14149b9b9880fd66fa67de5
Dorulkk <90566844+Dorulkk@users.noreply.github.com>
2021-09-20 23:59:34 +0300

    Add files via upload

---
+++ komutlar/yardım.js
@@ -0,0 +1,109 @@
+const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json')
+
+exports.run = async (client, message, args) => {
+    var prefix = ayarlar.prefix;
+
+
+    if(args[0] === "Genel" || args[0] === "genel" || args[0] === "General" || args[0] === "general") {
+              let Genel = new Discord.MessageEmbed()
+  .setAuthor('Genel', message.author.displayAvatarURL())
+  .setColor('#2667FF')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Genel').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+
+              return message.channel.send(Genel)
+
+
+       return;
+    }
+
+      if(args[0] === "Sunucu" || args[0] === "sunucu") {
+              let Sunucu = new Discord.MessageEmbed()
+  .setAuthor('Sunucu', message.author.displayAvatarURL())
+  .setColor('#2667FF')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Sunucu').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+
+              return message.channel.send(Sunucu)
+
+
+
+       return;
+    }
+
+  if(args[0] === "Eğlence" || args[0] === "eğlence" || args[0] === "Fun" || args[0] === "fun") {
+   let Eğlence = new Discord.MessageEmbed()
+  .setAuthor('Eğlence', message.author.displayAvatarURL())
+  .setColor('#2667FF')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Eğlence').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+
+   return message.channel.send(Eğlence)
+
+
+           return;
+  }
+  //MODERASYON KOMUTU
+  if(args[0] === "müzik" || args[0] === "Müzik" || args[0] === "music" || args[0] === "Music") {
+   let Müzik = new Discord.MessageEmbed()
+  .setAuthor('müzik', message.author.displayAvatarURL())
+  .setColor('#2667FF')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'müzik').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+
+   return message.channel.send()
+
+
+               return;
+  }
+     //SAHİP KOMUTU
+  if(args[0] === "Sahip" || args[0] === "sahip" ) {
+    let Sahip = new Discord.MessageEmbed()
+   .setAuthor('Moderasyon', message.author.displayAvatarURL())
+   .setColor('#2667FF')
+   .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Sahip').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+
+    return message.channel.send(Sahip)
+
+
+                return;
+   }
+
+//YARDIM KOMUTU
+
+  let embed = new Discord.MessageEmbed()
+  .setAuthor('Yardım Komutları', message.author.displayAvatarURL())
+  .setThumbnail(client.user.avatarURL())
+  .setColor('#FFFB05')
+  .setDescription(`**Örnek Kullanım:** \`${prefix}yardım Kategori\` \n **Örnek:** \`${prefix}yardım Genel\``)
+  .addField('Kategoriler:', `
+  **[${prefix}yardım Genel]()**
+  **[${prefix}yardım Eğlence]()**
+  **[${prefix}yardım Müzik]()**
+  **[${prefix}yardım Sunucu]()**
+  **[${prefix}yardım Sahip]()**
+  `)
+
+
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+  message.channel.send(embed)
+
+
+}
+
+
+
+  exports.conf = {
+    aliases: ['help', 'cmds', 'komutlar','y'], //Komutun farklı yazılışlarla kullanımları
+    permLevel: 0, //Komutun kimler kullanacağını belirtir (bot.js dosyasından en aşağı inerseniz gerekli yeri görürsünüz)
+    kategori: "Genel" //Yardım komutunda hangi kategoride gözükeceğini ayarlarsınız
+
+  };
+
+  exports.help = {
+    name: 'yardım',  //adını belirtin (kullanmak için gereken komut) Örneğin Otorol
+    description: 'Komutlar hakkında bilgi verir.', //Komutun açıklaması
+    usage: 'yardım', //Komutun kullanım şekli (örneğin !otorol <@rol> <#kanal>)
+  };
